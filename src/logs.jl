3c50ee09543a6154a19aadb7de27b2ad63d300c6
Yujie Wang <jesiner@gmail.com>
2022-08-24 19:38:12 -0700

    refactor email function

--- src/logs.jl
+++ src/logs.jl
@@ -7,6 +7,38 @@
 #######################################################################################################################################################################################################
 """
 
+    send_email!(subject::String, from_email::String, to_email::String, body::String)
+
+Send out email, given
+- `subject` Email subject
+- `from_email` The outgoing email address
+- `to_email` Email address to send out
+- `body` Main body of the email
+
+"""
+function send_email!(subject::String, from_email::String, to_email::String, body::String)
+    @info tinfo("Sending email to $(to_email)...");
+
+    # compose email and write the email to local drive
+    Base.write(".tmp_email", "Subject: $(subject)\nFrom: $(from_email)\nTo: $(to_email)\n$(body)");
+
+    # send the email through pipeline and delete the file
+    run(pipeline(".tmp_email", `sendmail -t`));
+    rm(".tmp_email");
+
+    return nothing
+end
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to these functions
+# General
+#     2022-Aug-24: move function outside of the folder
+#
+#######################################################################################################################################################################################################
+"""
+
     terror(info::String)
 
 Add a time tag to logging string, given
