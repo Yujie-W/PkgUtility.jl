33066b89190e624a6769d711c16d4e275c0416ff
Yujie Wang <jesiner@gmail.com>
2022-10-17 21:07:55 -0700

    add quad functions

--- src/statistics.jl
+++ src/statistics.jl
@@ -71,3 +71,63 @@ numerical∫(f::Function, x_min::FT, x_max::FT, x_tol::FT = sqrt(eps(FT)), y_tol
 
     return _sum_N
 );
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to the function
+# General
+#     2022-Oct-17: move function outside of the folder
+#
+#######################################################################################################################################################################################################
+"""
+
+    lower_quadratic(a::FT, b::FT, c::FT) where {FT<:AbstractFloat}
+
+Return the lower quadratic solution or NaN, given
+- `a` Parameter in `a*x^2 + b*x + c = 0`
+- `b` Parameter in `a*x^2 + b*x + c = 0`
+- `c` Parameter in `a*x^2 + b*x + c = 0`
+
+"""
+function lower_quadratic(a::FT, b::FT, c::FT) where {FT<:AbstractFloat}
+    discr = b^2 - 4*a*c;
+
+    if (discr >= 0) && (a > 0)
+        return (-b - sqrt(discr)) / (2 * a)
+    elseif (discr >= 0) && (a < 0)
+        return (-b + sqrt(discr)) / (2 * a)
+    else
+        return FT(NaN)
+    end
+end
+
+
+#######################################################################################################################################################################################################
+#
+# Changes to the function
+# General
+#     2022-Oct-17: move function outside of the folder
+#
+#######################################################################################################################################################################################################
+"""
+
+    upper_quadratic(a::FT, b::FT, c::FT) where {FT<:AbstractFloat}
+
+Return the upper quadratic solution or NaN, given
+- `a` Parameter in `a*x^2 + b*x + c = 0`
+- `b` Parameter in `a*x^2 + b*x + c = 0`
+- `c` Parameter in `a*x^2 + b*x + c = 0`
+
+"""
+function upper_quadratic(a::FT, b::FT, c::FT) where {FT<:AbstractFloat}
+    discr = b^2 - 4*a*c;
+
+    if (discr >= 0) && (a > 0)
+        return (-b + sqrt(discr)) / (2 * a)
+    elseif (discr >= 0) && (a < 0)
+        return (-b - sqrt(discr)) / (2 * a)
+    else
+        return FT(NaN)
+    end
+end
