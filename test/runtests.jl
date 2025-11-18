8e3462f9332adc75b7ee7fac6470f7f11b2e3067
Yujie <jesiner@gmail.com>
2025-11-18 10:24:16 +0800

    add RecursiveTools tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 
 @testset "EmeraldUtilities" verbose = true begin
-    # include("ArtifactTools.jl");
-    # include("DistributedTools.jl");
+    include("ArtifactTools.jl");
+    include("DistributedTools.jl");
     include("PrettyDisplay.jl");
 end;
